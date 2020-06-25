079ecb2f7e0903ca98bdbcec621fce50d79f37b7
Nikolas <nikolas.burk@gmail.com>
2020-06-25 11:23:04 +0200

    Update schema-3.sql

--- sql/schema-3.sql
+++ sql/schema-3.sql
@@ -1,6 +1,7 @@
 ALTER TABLE "public"."_CategoryToPost" RENAME COLUMN "A" TO "categoryId";
 ALTER TABLE "public"."_CategoryToPost" RENAME COLUMN "B" TO "postId";
 ALTER TABLE "public"."_CategoryToPost"
-  ADD COLUMN "userId" integer,
-  ADD UNIQUE ("userId"),
+  ADD COLUMN "userId" INTEGER,
   ADD FOREIGN KEY ("userId") REFERENCES "public"."User"("id");
+ALTER TABLE "public"."_CategoryToPost"
+  ADD COLUMN "createdAt" timestamp NOT NULL DEFAULT now();
