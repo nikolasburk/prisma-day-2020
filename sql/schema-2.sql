07a8fa1e27155a75d34781a253137899f677d262
Nikolas <nikolas.burk@gmail.com>
2020-06-25 11:23:38 +0200

    Update schema-2.sql

--- sql/schema-2.sql
+++ sql/schema-2.sql
@@ -3,8 +3,8 @@ CREATE TABLE "public"."Category" (
   name VARCHAR(255)
 );
 CREATE TABLE "_CategoryToPost" (
-    "A" integer NOT NULL REFERENCES "Category"(id),
-    "B" integer NOT NULL REFERENCES "Post"(id)
+    "A" INTEGER NOT NULL REFERENCES "Category"(id),
+    "B" INTEGER NOT NULL REFERENCES "Post"(id)
 );
-CREATE UNIQUE INDEX "_CategoryToPost_AB_unique" ON "_CategoryToPost"("A" int4_ops,"B" int4_ops);
-CREATE INDEX "_CategoryToPost_B_index" ON "_CategoryToPost"("B" int4_ops);
+CREATE UNIQUE INDEX "_CategoryToPost_AB_unique" ON "_CategoryToPost"("A","B");
+CREATE INDEX "_CategoryToPost_B_index" ON "_CategoryToPost"("B");
